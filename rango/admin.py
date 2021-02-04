a299fac00fd411463538c081a193e53f6806f6e0
rishabhmathur23 <77937795+rishabhmathur23@users.noreply.github.com>
2021-02-05 03:22:36 +0530

    Update admin.py

--- rango/admin.py
+++ rango/admin.py
@@ -3,7 +3,11 @@ from rango.models import Category, Page
 
 # Register your models here.
 
-admin.site.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug' : ('name',)}
+
+admin.site.register(Category, CategoryAdmin)
+
 
 class PageAdmin(admin.ModelAdmin):
     list_display = ('title', 'category', 'url')
